f0626b01ed438335b7d8e79f91063a16f6dceaac
Developer Mazharul <mazharulweb@gmail.com>
2021-02-02 21:14:18 +0600

    null-vs-undefned

---
+++ null-vs-undefiend.js
@@ -0,0 +1,23 @@
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+const result = add(13, 80);
+console.log(result);
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const result2 = add(13);
+console.log(result2);
+
+const premik = {name:"smart dude", phone:45785};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 8];
+console.log(ages[11]);
